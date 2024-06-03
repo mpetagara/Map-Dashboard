58c01568a38de7220fed798225a2b6a12a7e4726
mpetagara <matthew.a.petagara.ctr@mail.mil>
2024-06-03 08:16:05 -0500

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,1 +1,10 @@
-print(True)
+import json, os, glob 
+
+entries = []
+
+for filename in glob.glob(os.path.join('data/', '*.json')):
+    with open(filename, encoding='utf-8', mode='r') as currentFile:
+      data = json.loads(currentFile.read())
+      for i in data['entries]:
+        entries.append(i)
+print(len(entries))
